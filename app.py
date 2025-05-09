e78294f07ec79adf07793f05357139a7b21e8635
Charles Lukowski <lukowski.2@gmail.com>
2025-05-08 21:05:20 -0400

    debug deploy

--- app.py
+++ app.py
@@ -179,7 +179,7 @@ def index():
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
